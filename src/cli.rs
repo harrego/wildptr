9b215b3e0bdcd98adbfed7cf78853b7cbcc000e1
Harry Stanton <h@harry.city>
2021-03-08 20:38:03 +0000

    feat: added cli parsing and argument executing

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,11 @@
+use substring::Substring;
+use std::process;
+
 pub struct Argument {
   pub name: String,
   pub description: String,
 
-  pub callback: fn(String),
+  pub callback: fn(),
 }
 
 pub struct Flag {
@@ -13,6 +16,7 @@ pub struct Flag {
 pub struct CLI {
   pub app_name: String,
   pub app_version: String,
+  pub app_description: String,
 
   pub arguments: Vec<Argument>,
   pub flags: Vec<Flag>,
@@ -20,7 +24,7 @@ pub struct CLI {
 impl CLI {
   pub fn print_help(&self) {
     println!("{} v{}", self.app_name, self.app_version);
-    println!("(c) harrego");
+    println!("{}", self.app_description);
 
     print!("\nusage: wildptr ");
     for flag in &self.flags {
@@ -44,5 +48,59 @@ impl CLI {
       println!("    {} - {}", arg.name, arg.description);
     }
   }
-}
 
+  fn handle_arg(&self, arg: &String) {
+    println!("handling arg: {}", arg);
+  }
+
+  pub fn parse(&self) {
+    let mut selected_cli_argument: Option<&Argument> = None;
+
+    let args = std::env::args();
+    for (index, arg) in args.enumerate() {
+      if index == 0 {
+        continue;
+      }
+      let mut chars = arg.chars();
+      if chars.next() == Some('-') {
+        let dash_suffix = chars.next();
+        if dash_suffix == Some('-') {
+          // double loaded long flag 
+
+          // unsure if using .count() at the end of the substring
+          // method is safe for unicode, are chars() byte or ascii
+          // based?
+          self.handle_arg(&arg.substring(2, arg.chars().count()).to_string());
+        } else if let Some(unhandled_arg) = dash_suffix {
+          // single dash multi loaded flag
+          self.handle_arg(&unhandled_arg.to_string());
+          loop {
+            match chars.next() {
+              Some(val) => self.handle_arg(&val.to_string()),
+              None => break,
+            }
+          }
+        }
+      } else if !selected_cli_argument.is_none() {
+        println!("error: more than one argument provided");
+      } else {
+        for cli_argument in &self.arguments {
+          if cli_argument.name == arg {
+            selected_cli_argument = Some(cli_argument);
+            break
+          } else {
+            println!("error: unrecognized argument \"{}\"", arg);
+            println!("       use -h to list available arguments");
+            process::exit(1);
+          }
+        }
+      }
+    }
+    if let Some(arg) = selected_cli_argument {
+      (arg.callback)();
+    } else {
+      println!("error: no argument given");
+      println!("       use -h to list available arguments");
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod cli;
 use crate::cli::*;
 
-fn list_arg(x: String) {
-  println!("list argument: {}", x);
+fn list_arg() {
+  println!("list argument called");
 }
 
 fn main() {
   let args = vec![Argument { name: "list".to_string(), description: "list all RFCs".to_string(), callback: list_arg } ];
   let flags = vec![Flag { flags: vec!["v".to_string(), "verbose".to_string()], description: "verbose mode".to_string() }];
-  let cli = CLI { app_name: "wildptr".to_string(), app_version: "1.0.0".to_string(), arguments: args, flags: flags }; 
-  cli.print_help();
+  let cli = CLI { app_name: "wildptr".to_string(), app_version: "1.0.0".to_string(), app_description: "(c) harrego".to_string(), arguments: args, flags: flags }; 
+  cli.parse();
 }
